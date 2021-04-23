6306077f14563e098982f39a50b428af04038397
timnik <72356939+nottimnik@users.noreply.github.com>
2021-04-23 23:50:43 +0300

    Added contact info and art page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -11,7 +11,9 @@ export default function Home() {
             {' '}
             <Link to="/projects" style={{top:"30px", position:"relative", display: "flex", justifyContent: "center"}}>My Projects</Link>
             {' '}
-            <Link to="/games" style={{top:"40px", position:"relative", display: "flex", justifyContent: "center"}}>My Games</Link>
+            <Link to="/art" style={{top:"40px", position:"relative", display: "flex", justifyContent: "center"}}>My Art</Link>
+            {' '}
+            <Link to="/games" style={{top:"50px", position:"relative", display: "flex", justifyContent: "center"}}>My Games</Link>
 
             <h2 style={{position:"relative" ,top: "360px",fontFamily: "fantasy", textAlign: "center"}}>Contact Me</h2>
             <p style={{fontSize: "13px"  ,position:"relative" ,top: "362px",fontFamily: "fantasy", textAlign: "center"}}>
@@ -20,6 +22,7 @@ export default function Home() {
 
             <h6 style={{position:"relative" ,top: "380px" ,fontFamily: "fantasy", textAlign: "center"}}>I might create a blog tho, <br /> kinda wanna share some thoughts</h6>
 
+            
         </div>
     )
 }
