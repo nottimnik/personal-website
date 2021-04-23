cead065e358583276b1b699c15c3cc124a78d6ad
timnik <72356939+nottimnik@users.noreply.github.com>
2021-04-23 16:46:07 +0300

    Created routes for all the pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import { BrowserRouter as Router ,Route, Switch } from 'react-router-dom'
 import Home from './pages/Home'
+import About from './pages/About'
+import Projects from './pages/Projects'
+import Art from './pages/Art'
+import Games from './pages/Games'
 
 function App() {
   return (
     <Router> 
       <Switch>
-        <Route path="/" component={Home} />
+        <Route exact path="/" component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/projects" component={Projects} />
+        <Route path="/art" component={Art} />
+        <Route path="/games" component={Games} />
       </Switch>
     </Router>
   );
