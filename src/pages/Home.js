766c437fd2337dd463d9683e4a4b1c5cbbc29752
timnik <72356939+nottimnik@users.noreply.github.com>
2021-04-23 22:40:09 +0300

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -11,9 +11,15 @@ export default function Home() {
             {' '}
             <Link to="/projects" style={{top:"30px", position:"relative", display: "flex", justifyContent: "center"}}>My Projects</Link>
             {' '}
-            <Link to="/art" style={{top:"40px", position:"relative", display: "flex", justifyContent: "center"}}>My Art</Link>
-            {' '}
-            <Link to="/games" style={{top:"50px", position:"relative", display: "flex", justifyContent: "center"}}>My Games</Link>
+            <Link to="/games" style={{top:"40px", position:"relative", display: "flex", justifyContent: "center"}}>My Games</Link>
+
+            <h2 style={{position:"relative" ,top: "360px",fontFamily: "fantasy", textAlign: "center"}}>Contact Me</h2>
+            <p style={{fontSize: "13px"  ,position:"relative" ,top: "362px",fontFamily: "fantasy", textAlign: "center"}}>
+                0771 351 758 <br />
+                calin.petru.ungureanu@gmail.com
+            </p>
+
+            <h6 style={{position:"relative" ,top: "380px" ,fontFamily: "fantasy", textAlign: "center"}}>I might create a blog tho, <br /> kinda wanna share some thoughts</h6>
 
         </div>
     )
