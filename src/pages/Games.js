53fa8aeec8778ee3ff51d656455e83825a71b69c
timnik <72356939+nottimnik@users.noreply.github.com>
2021-04-25 00:28:37 +0300

    Added link to the tutorial

--- src/pages/Games.js
+++ src/pages/Games.js
@@ -8,6 +8,7 @@ export default function Games() {
             <p style={{fontFamily:"fantasy"}} >A simple game where your goal is to become rich, by mining stone.</p>   
             <a target="_blank" href="https://github.com/nottimnik/stone-miner">Github</a>
             {' '}
+            <a href="/stone-miner/tutorial">Tutorial</a>
 
             <Link to="/" style={{top:"20px", position:"relative", display: "flex", justifyContent: "center"}}>Go back.</Link>
         </div>
