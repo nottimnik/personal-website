21d7fd510a5be3e63a2f727502d7e2763c859a55
timnik <72356939+nottimnik@users.noreply.github.com>
2021-04-25 16:01:33 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,8 @@
-import { BrowserRouter as Router ,Route, Switch } from 'react-router-dom'
-import Home from './pages/Home'
-import About from './pages/About'
-import Projects from './pages/Projects'
-import Art from './pages/Art'
-import Games from './pages/Games'
-import StoneMinerTutorial from './pages/Games/StoneMinerTutorial'
+import React from 'react'
+import HomeNavbar from './../components/Navbar/HomeNavbar'
 
-function App() {
-  return (
-    <Router> 
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route path="/about" component={About} />
-        <Route path="/projects" component={Projects} />
-        <Route path="/art" component={Art} />
-        <Route path="/games" component={Games} />
-        <Route path="/stone-miner/tutorial" component={StoneMinerTutorial} /> 
-      </Switch>
-    </Router>
-  );
-}
-
-export default App;
+export default function Home() {
+    return (
+        <>
+            <HomeNavbar />
+        </>
